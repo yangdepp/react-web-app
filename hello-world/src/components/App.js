8c07e30650e0b4a239efb9cfe609c9ed05aa53ee
yangdepp <965093975@qq.com>
2019-03-23 20:45:30 +0800

    项目

--- hello-world/src/components/App.js
+++ hello-world/src/components/App.js
@@ -1,13 +1,19 @@
 import React, {Component} from 'react';
+import AddTodo from './AddTodo'
+import Footer from './Footer'
+import TodoList from './TodoList'
+
 
 class App extends Component {
   render() {
     return (
-        < div >
-          hello world
-        < /div>
-  )
-    ;
+        <div>
+          <AddTodo/>
+          <TodoList/>
+          <Footer/>
+        </div>
+    )
+        ;
   }
 }
 
