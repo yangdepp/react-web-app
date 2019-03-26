090bed738a3d2fadbd5fbfd377eb2ed8fd85da0d
yangdepu <yangdepu@zdsoft.cn>
2019-03-26 17:33:13 +0800

    开始写一个redux使用文档

--- redux-todo-list/src/containers/FooterContainer.js
+++ redux-todo-list/src/containers/FooterContainer.js
@@ -1,7 +1,6 @@
 import {connect} from 'react-redux'
 import Footer from "../components/Footer";
 import {setFilter} from "../actions";
-import filter from "../reducers/filter";
 
 const mapStateToProps = state => ({
   filter: state.filter
--- redux-todo-list/src/store.js
+++ redux-todo-list/src/store.js
@@ -1,22 +1,22 @@
-import {createStore} from 'redux'
-import rootReducer from './reducers'
-import {addTodo, toggleTodo, setTodoText, setFilter} from './actions/index'
+// import {createStore} from 'redux'
+// import rootReducer from './reducers'
+// import {addTodo, toggleTodo, setTodoText, setFilter} from './actions/index'
 
-const store = createStore(rootReducer)
+// const store = createStore(rootReducer)
 
-//  初始state
-console.log('000')
-console.log(store.getState())
+// //  初始state
+// console.log('000')
+// console.log(store.getState())
 
-//  订阅state变化
-const unsubscribe = store.subscribe(() => {
-  console.log(store.getState())
-})
+// //  订阅state变化
+// const unsubscribe = store.subscribe(() => {
+//   console.log(store.getState())
+// })
 
-//  发送actions
-store.dispatch(addTodo('learn actions'))
-store.dispatch(toggleTodo(0))
-store.dispatch(setFilter('active'))
-store.dispatch(setTodoText('learn'))
+// //  发送actions
+// store.dispatch(addTodo('learn actions'))
+// store.dispatch(toggleTodo(0))
+// store.dispatch(setFilter('active'))
+// store.dispatch(setTodoText('learn'))
 
-unsubscribe()
+// unsubscribe()
