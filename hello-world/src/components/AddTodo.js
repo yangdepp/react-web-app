5641e42140ff03d414304749cc3653b0fd71659b
yangdepu <yangdepu@zdsoft.cn>
2019-03-26 14:29:56 +0800

    使用自己发布的包

--- hello-world/src/components/AddTodo.js
+++ hello-world/src/components/AddTodo.js
@@ -1,4 +1,5 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { Icon } from 'react-golu'
 
 class AddTodo extends Component {
   constructor(props) {
@@ -19,13 +20,18 @@ class AddTodo extends Component {
       text: e.target.value
     })
   }
+  clickIcon(){
+    console.log('111')
+  }
 
   render() {
     return (
-        <div>
-          <input type="text" value={this.state.text} onChange={this.handleChange}/>
-          <button onClick={this.handleClick}>Add</button>
-        </div>
+      <div>
+        <input type="text" value={this.state.text} onChange={this.handleChange} />
+        <button onClick={this.handleClick}>Add</button>
+        <Icon name="wechat" onClick={this.clickIcon.bind(this)}></Icon>
+        <Icon name="alipay"></Icon>
+      </div>
     );
   }
 }
