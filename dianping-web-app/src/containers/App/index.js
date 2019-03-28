5155402ccf694a8888621aa227f158be9033ebe0
yangdepu <yangdepu@zdsoft.cn>
2019-03-28 16:53:28 +0800

    redux模块分层

--- dianping-web-app/src/containers/App/index.js
+++ dianping-web-app/src/containers/App/index.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import './style.css'
 
 class App extends Component {
   render() {
     return (
       <div>
-        App
+        <p>ppppp</p>
       </div>
     );
   }
