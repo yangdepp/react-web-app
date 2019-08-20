730b95ce2adf103788f78b3f0907a73287798a69
yangdepu <yangdepp@163.com>
2019-08-20 18:03:40 +0800

    redux

---
+++ to-do-react/src/actions/index.js
@@ -0,0 +1,27 @@
+import {ADD_TODO, TOGGLE_TODO, SET_FILTER, SET_TODO_TEXT} from './actionTypes';
+let nextTodoId = 0;
+
+// 新增代办事项
+export const addTodo = (text) => ({
+  type: ADD_TODO,
+  id: nextTodoId++,
+  text,
+})
+
+// 更改代办事项状态
+export const toggleTodo = id => ({
+  type: TOGGLE_TODO,
+  id
+})
+
+// 设置过滤状态
+export const setFilter = filter => ({
+  type: SET_FILTER,
+  filter,
+})
+
+//  设置新增文本
+export const setTodoText = text => ({
+  type: SET_TODO_TEXT,
+  text
+})
---
+++ to-do-react/src/reducers/todos.js
@@ -0,0 +1,27 @@
+import { ADD_TODO, TOGGLE_TODO } from '../actions/actionTypes';
+
+const todos = (state = [], action) => {
+  switch (action.type) {
+    case ADD_TODO:
+      return [
+        ...state,
+        {
+          id: action.id,
+          text: action.text,
+          completed: false,
+        },
+      ];
+    case TOGGLE_TODO:
+      return state.map((item) =>
+        item.id === action.id
+          ? {
+              ...item,
+              completed: !item.completed,
+            }
+          : item,
+      );
+    default:
+      return state;
+  }
+};
+export default todos;
